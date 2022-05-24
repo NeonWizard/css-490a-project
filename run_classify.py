31afdb99c0f0db95f2c1bfcf83dd7535a2b2d65c
Wes <averagewizard13@gmail.com>
2022-05-24 16:36:33 -0700

    formatting updates

--- run_classify.py
+++ run_classify.py
@@ -1,6 +1,6 @@
 from keras.applications.vgg16 import VGG16
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg16 import preprocess_input,decode_predictions
+from tensorflow.keras.applications.vgg16 import preprocess_input, decode_predictions
 import numpy as np
 
 model = VGG16(weights='imagenet')
@@ -11,13 +11,12 @@ img_path = 'images/dog.png'
 #image loaded in PIL (Python Imaging Library)
 img = image.load_img(img_path,color_mode='rgb', target_size=(224, 224))
 
-# Converts a PIL Image to 3D Numy Array
+# Converts a PIL Image to 3D Numpy Array
 x = image.img_to_array(img)
-x.shape
 # Adding the fouth dimension, for number of images
 x = np.expand_dims(x, axis=0)
 
-#mean centering with respect to Image
+# mean centering with respect to Image
 x = preprocess_input(x)
 features = model.predict(x)
 p = decode_predictions(features)
