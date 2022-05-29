81c112efbb6f6168801766b44323eb8a684da5f1
Wes <averagewizard13@gmail.com>
2022-05-28 22:02:46 -0700

    Undo OOP style and finish pruning

--- main.py
+++ main.py
@@ -42,7 +42,175 @@ def f1_m(y_true, y_pred):
 # plt.legend(["Accuracy","Validation Accuracy","Loss","Validation Loss"])
 # plt.savefig("graph_acc_loss.png")
 
-class MyVGG16:
+def build_vgg16(num_classes):
+  # -- build model
+  model = Sequential()
+
+  # 2x convolution layer of 64 channel of 3x3 kernal and same padding
+  model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+  model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
+
+  # 1x maxpool layer of 2x2 pool size and stride 2x2
+  model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+  # 2x convolution layer of 128 channel of 3x3 kernal and same padding
+  model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+  model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+
+  # 1x maxpool layer of 2x2 pool size and stride 2x2
+  model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+  # 3x convolution layer of 256 channel of 3x3 kernal and same padding
+  model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+  model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+  model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+
+  # 1x maxpool layer of 2x2 pool size and stride 2x2
+  model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+  # 3x convolution layer of 512 channel of 3x3 kernal and same padding
+  model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+  model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+  model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+
+  # 1x maxpool layer of 2x2 pool size and stride 2x2
+  model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+  # 3x convolution layer of 512 channel of 3x3 kernal and same padding
+  model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+  model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+  model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+
+  # 1x maxpool layer of 2x2 pool size and stride 2x2
+  model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+
+  # Fully connected layers
+  model.add(Flatten())
+  model.add(Dense(units=4096,activation="relu"))
+  model.add(Dense(units=4096,activation="relu"))
+  model.add(Dense(units=num_classes, activation="softmax"))
+
+  return model
+
+def normalize(self, x_train, x_test):
+  # this function normalize inputs for zero mean and unit variance
+  # it is used when training a model.
+  # Input: training set and test set
+  # Output: normalized training set and test set according to the trianing set statistics.
+  mean = np.mean(x_train,axis=(0,1,2,3))
+  std = np.std(x_train, axis=(0, 1, 2, 3))
+  x_train = (x_train-mean)/(std+1e-7)
+  x_test = (x_test-mean)/(std+1e-7)
+  return x_train, x_test
+
+def predict(model, x, batch_size=50):
+  return model.predict(x, batch_size)
+
+def train(model, train_data, test_data, learning_rate, epochs):
+  # training parameters
+  batch_size = 32
+
+  # -- Compile model
+  opt = adam_v2.Adam(learning_rate=learning_rate)
+  model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy', f1_m, precision_m, recall_m])
+
+  # model.summary()
+
+  # -- Model fitting
+  checkpoint = ModelCheckpoint("vgg16.h5", monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+  early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=1, mode='auto')
+
+  start_time = time.time()
+  hist = model.fit(
+    train_data,
+    validation_data=test_data,
+    # steps_per_epoch=len(train_data) // batch_size,
+    epochs=epochs,
+    # validation_steps=10,
+    # batch_size=batch_size,
+    callbacks=[checkpoint,early],
+    verbose=2)
+
+  print()
+  print("--- Training Time ---")
+  print("%s seconds" % round(time.time() - start_time, 3))
+  print()
+
+  return model, hist
+
+def test(model, test_data):
+  loss, acc, f1_score, precision, recall = model.evaluate(test_data)
+
+  print("Loss: {}\nAcc: {}\nF1 Score: {}\nPrecision: {}\nRecall: {}".format(
+    round(loss*100, 3),
+    round(acc*100, 3),
+    round(f1_score*100, 3),
+    round(precision*100, 3),
+    round(recall*100, 3),
+  ))
+
+  return loss, acc, f1_score, precision, recall
+
+def predict(model, img):
+  start_time = time.time()
+  prediction_ind = model.predict(img).argmax(axis=-1)
+  prediction = np.array(["paper", "rock", "scissors"])[prediction_ind][0]
+
+  print()
+  print("--- Single Image Inference Time ---")
+  print("%s seconds" % round(time.time() - start_time, 3))
+  print()
+
+  print()
+  print("Prediction: " + prediction)
+  print()
+
+  return prediction_ind
+
+def prune(model, train_data, test_data, learning_rate, epochs, batch_size):
+  end_step = np.ceil(1.0 * len(train_data) / batch_size).astype(np.int32) * epochs
+  print("End step:", end_step)
+
+  new_pruning_params = {
+    'pruning_schedule': sparsity.PolynomialDecay(initial_sparsity=0.30,
+                                                final_sparsity=0.70,
+                                                begin_step=0,
+                                                end_step=end_step)
+                                                # frequency=100)
+  }
+
+  new_pruned_model = sparsity.prune_low_magnitude(model, **new_pruning_params)
+
+  opt = adam_v2.Adam(learning_rate=learning_rate)
+  new_pruned_model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy', f1_m, precision_m, recall_m])
+  # new_pruned_model.summary()
+
+  # -- Model fitting
+  callbacks = [
+    sparsity.UpdatePruningStep()
+  ]
+
+  start_time = time.time()
+  hist = new_pruned_model.fit(
+    train_data,
+    validation_data=test_data,
+    # steps_per_epoch=len(train_data) // batch_size,
+    epochs=epochs,
+    # validation_steps=10,
+    # batch_size=batch_size,
+    callbacks=callbacks)
+
+  stripped_model = sparsity.strip_pruning(new_pruned_model)
+  keras.models.save_model(stripped_model, "vgg16-pruned.h5")
+  print("Saved pruned Keras model to vgg16-pruned.h5")
+  # stripped_model.summary()
+
+  return new_pruned_model, stripped_model
+
+
+def main():
+  should_train = True
+
   # -- load images
   id_generator = ImageDataGenerator(
       featurewise_center=False,  # set input mean to 0 over the dataset
@@ -62,234 +230,47 @@ class MyVGG16:
   test_data = id_generator.flow_from_directory(
     directory="data/test", target_size=(224, 224))
 
-  def __init__(self, train=True):
-    self.num_classes = 3
-    self.learning_rate = 0.0001
-    self.weight_decay = 0.0005
-    self.x_shape = [32, 32, 3]
+  num_classes = 3
+  learning_rate = 0.0001
+  x_shape = [32, 32, 3]
 
-    if train:
-      self.model = self.build_model()
-      self.train()
-    else:
-      self.model = load_model("vgg16.h5", custom_objects={
-        "f1_m": f1_m,
-        "precision_m": precision_m,
-        "recall_m": recall_m
-      })
+  if should_train:
+    model = build_vgg16(num_classes)
+    model, hist = train(model, train_data, test_data, learning_rate, epochs=10)
+  else:
+    model = load_model("vgg16.h5", custom_objects={
+      "f1_m": f1_m,
+      "precision_m": precision_m,
+      "recall_m": recall_m
+    })
 
-  def build_model(self):
-    # -- build model
-    model = Sequential()
-
-    # 2x convolution layer of 64 channel of 3x3 kernal and same padding
-    model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-    model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-
-    # 1x maxpool layer of 2x2 pool size and stride 2x2
-    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-    # 2x convolution layer of 128 channel of 3x3 kernal and same padding
-    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-
-    # 1x maxpool layer of 2x2 pool size and stride 2x2
-    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-    # 3x convolution layer of 256 channel of 3x3 kernal and same padding
-    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-
-    # 1x maxpool layer of 2x2 pool size and stride 2x2
-    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-    # 3x convolution layer of 512 channel of 3x3 kernal and same padding
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-
-    # 1x maxpool layer of 2x2 pool size and stride 2x2
-    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-    # 3x convolution layer of 512 channel of 3x3 kernal and same padding
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-    model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-
-    # 1x maxpool layer of 2x2 pool size and stride 2x2
-    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-
-    # Fully connected layers
-    model.add(Flatten())
-    model.add(Dense(units=4096,activation="relu"))
-    model.add(Dense(units=4096,activation="relu"))
-    model.add(Dense(units=self.num_classes, activation="softmax"))
-
-    return model
-
-  def normalize(self, x_train, x_test):
-    # this function normalize inputs for zero mean and unit variance
-    # it is used when training a model.
-    # Input: training set and test set
-    # Output: normalized training set and test set according to the trianing set statistics.
-    mean = np.mean(x_train,axis=(0,1,2,3))
-    std = np.std(x_train, axis=(0, 1, 2, 3))
-    x_train = (x_train-mean)/(std+1e-7)
-    x_test = (x_test-mean)/(std+1e-7)
-    return x_train, x_test
-
-  # def normalize_production(self, x):
-  #   # this function is used to normalize instances in production according to saved training set statistics
-  #   # Input: X - a training set
-  #   # Output X - a normalized training set according to normalization constants.
-
-  #   # these values produced during first training and are general for the standard cifar10 training set normalization
-  #   mean = 120.707
-  #   std = 64.15
-  #   return (x-mean)/(std+1e-7)
-
-  def predict(self, x, normalize=True, batch_size=50):
-    if normalize:
-      x = self.normalize_production(x)
-
-    return self.model.predict(x,batch_size)
-
-  def train(self):
-    # training parameters
-    batch_size = 32
-    max_epochs = 10
-
-    # -- Compile model
-    opt = adam_v2.Adam(learning_rate=self.learning_rate)
-    self.model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy', f1_m, precision_m, recall_m])
-
-    self.model.summary()
-
-    # -- Model fitting
-    checkpoint = ModelCheckpoint("vgg16.h5", monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
-    early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=1, mode='auto')
-
-    start_time = time.time()
-    hist = self.model.fit(
-      self.train_data,
-      validation_data=self.test_data,
-      # steps_per_epoch=len(train_data) // batch_size,
-      epochs=max_epochs,
-      # validation_steps=10,
-      # batch_size=batch_size,
-      callbacks=[checkpoint,early],
-      verbose=2)
-
-    print()
-    print("--- Training Time ---")
-    print("%s seconds" % round(time.time() - start_time, 3))
-    print()
-
-    return self.model
-
-  def test(self, test_data):
-    loss, acc, f1_score, precision, recall = self.model.evaluate(test_data)
-
-    print()
-    print("-- Evaluation on testing dataset --")
-    print("Loss: {}\nAcc: {}\nF1 Score: {}\nPrecision: {}\nRecall: {}".format(
-      round(loss*100, 3),
-      round(acc*100, 3),
-      round(f1_score*100, 3),
-      round(precision*100, 3),
-      round(recall*100, 3),
-    ))
-    print()
-
-  def predict(self, img):
-    start_time = time.time()
-    prediction_ind = self.model.predict(img).argmax(axis=-1)
-    prediction = np.array(["paper", "rock", "scissors"])[prediction_ind][0]
-
-    print()
-    print("--- Single Image Inference Time ---")
-    print("%s seconds" % round(time.time() - start_time, 3))
-    print()
-
-    print()
-    print("Prediction: " + prediction)
-    print()
-
-    return prediction_ind
-
-  def prune(self):
-    if self.model == None:
-      raise Exception("No model to prune.")
-
-    epochs = 10
-    batch_size = 32
-    end_step = np.ceil(1.0 * len(self.train_data) / batch_size).astype(np.int32) * epochs
-    print("End step:", end_step)
-
-    new_pruning_params = {
-      'pruning_schedule': sparsity.PolynomialDecay(initial_sparsity=0.30,
-                                                  final_sparsity=0.70,
-                                                  begin_step=0,
-                                                  end_step=end_step,
-                                                  frequency=100)
-    }
-
-    new_pruned_model = sparsity.prune_low_magnitude(self.model, **new_pruning_params)
-
-    opt = adam_v2.Adam(learning_rate=self.learning_rate)
-    new_pruned_model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy', f1_m, precision_m, recall_m])
-    # new_pruned_model.summary()
-
-    # -- Model fitting
-    callbacks = [
-      sparsity.UpdatePruningStep()
-    ]
-
-    start_time = time.time()
-    hist = new_pruned_model.fit(
-      self.train_data,
-      validation_data=self.test_data,
-      # steps_per_epoch=len(train_data) // batch_size,
-      epochs=epochs,
-      # validation_steps=10,
-      # batch_size=batch_size,
-      callbacks=callbacks)
-
-    loss, acc, f1_score, precision, recall = new_pruned_model.evaluate(self.test_data)
-
-    print()
-    print("-- Evaluation of PRUNED model on testing dataset --")
-    print("Loss: {}\nAcc: {}\nF1 Score: {}\nPrecision: {}\nRecall: {}".format(
-      round(loss*100, 3),
-      round(acc*100, 3),
-      round(f1_score*100, 3),
-      round(precision*100, 3),
-      round(recall*100, 3),
-    ))
-    print()
-
-    final_model = sparsity.strip_pruning(new_pruned_model)
-    keras.models.save_model(final_model, "vgg16-pruned.h5")
-    print("Saved pruned Keras model to vgg16-pruned.h5")
-    # final_model.summary()
-
-
-def main():
-  model = MyVGG16(train=False)
+  pruned_model, stripped_model = prune(
+    model,
+    train_data,
+    test_data,
+    learning_rate,
+    epochs=10,
+    batch_size=32)
 
   # -- Evaluate model
-  model.test(model.test_data)
+  print()
+  print("-- Testing BASELINE model --")
+  test(model, test_data)
+  print()
+
+  print()
+  print("-- Testing PRUNED model --")
+  test(pruned_model, test_data)
+  print()
 
   # -- Classify a single image
-  # img = image.load_img("data/validate/rock1.png", target_size=(224,224))
-  # img = np.asarray(img)
-  # img = np.expand_dims(img, axis=0)
+  img = image.load_img("data/validate/rock1.png", target_size=(224,224))
+  img = np.asarray(img)
+  img = np.expand_dims(img, axis=0)
 
-  # model.predict(img)
+  predict(model, img)
+  predict(pruned_model, img)
 
-  # -- Prune model
-  model.prune()
 
 if __name__ == "__main__":
   main()
